7fe8f0c87f5166c44bed95deb7e40fb8664276df
Namya0303 <35605470+Namya0303@users.noreply.github.com>
2020-06-20 00:37:29 +0530

    pata nahi

--- index.js
+++ index.js
@@ -23,23 +23,20 @@ jQuery(document).on("scroll", function () {
     }
 });
 
+/*
     // SMOOTH SCROLL
-
-$(document).ready(function(){
-    $("a").on('click', function(event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            var hash = this.hash;
-
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function(){
-                window.location.hash = hash;
-            });
-        }
-    });
+$("#navh").click(function() {
+    $("html, body").animate( 
+                { scrollTop: '0' }, 3000); 
 });
 
+var h = $('#about').scrollTop();
+
+$("#nava").click(function() {
+    $("html, body").animate( 
+                { scrollTop: h }, 3000); 
+}); */
+ 
     // COUNTDOWN
 
 var myfunc = setInterval(function() {
