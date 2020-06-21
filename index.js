2e098433a8adc33d4173766d4ea05a08a04d71a6
Naman Chandok <42838783+NamanChandok@users.noreply.github.com>
2020-06-21 17:30:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -83,3 +83,31 @@ function switchTheme(e) {
         document.documentElement.setAttribute('data-theme', 'dark');
     }
 }
+
+    // SAVE PREFERENCE TO LOCAL STORAGE
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dakr');
+    }
+}
+
+    // CHECK LOCAL STORAGE FOR PREFERENCE
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'light') {
+        toggleSwitch.checked = true;
+    }
+}
+
+    
