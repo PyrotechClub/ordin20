9f4a5432d4e8d21a616cdb63d9c7fa6662888c84
Naman Chandok <42838783+NamanChandok@users.noreply.github.com>
2020-06-27 18:05:42 +0530

    Revert "Update nav.js"
    This reverts commit 68c68b2c9aa9a2e829cecb406faa1898dbd073a4.

--- nav.js
+++ nav.js
@@ -1,4 +1,6 @@
-$(document).ready(function (){
+   
+
+   $(document).ready(function (){
     $("a.nav-img").click(function (){
         $('html, body').animate({
             scrollTop: $("#home").offset().top
