5d4983499965d6ce8fbb70c2aef0c4b1fa427a2a
Namya0303 <35605470+Namya0303@users.noreply.github.com>
2020-06-27 17:52:32 +0530

    ...

---
+++ nav.js
@@ -0,0 +1,49 @@
+   
+
+   $(document).ready(function (){
+    $("a.nav-img").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#home").offset().top
+        }, 2000);
+    });
+});
+
+$(document).ready(function (){
+    $("a.navt").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#heads").offset().top
+        }, 2000);
+    });
+});
+
+$(document).ready(function (){
+    $("a.nave").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#events").offset().top
+        }, 2000);
+    });
+});
+
+$(document).ready(function (){
+    $("a.navg").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#rules").offset().top
+        }, 2000);
+    });
+});
+
+$(document).ready(function (){
+    $("a.nava").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#about").offset().top
+        }, 2000);
+    });
+});
+
+$(document).ready(function (){
+    $("a.navh").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#home").offset().top
+        }, 2000);
+    });
+});
